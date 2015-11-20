64bab26222d1fefa948df2d4e2aee0958785d84d
Brad Nelson <bnelsj@gmail.com>
2015-11-20 11:12:53 -0800

    Add R/2.15.0 to module load

--- config.sh
+++ config.sh
@@ -2,9 +2,5 @@ module purge
 
 module load modules modules-init modules-gs/prod modules-eichler/prod
 
-module load samtools/1.1
-module load bwa/0.7.3
-module load samblaster/0.1.21
-module load java/7u17
-module load picard/1.111
-module load anaconda/2.1.0
+module load R/2.15.0 samtools/1.1 bwa/0.7.3 samblaster/0.1.21 java/7u17
+module load picard/1.111 anaconda/2.1.0
